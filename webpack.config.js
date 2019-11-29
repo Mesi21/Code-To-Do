cb301a6bf81674939443c5e72320aee57d2c7fa6
Mesi <mse212002@gmail.com>
2019-11-29 18:04:36 +0200

    :pushpin: Basic setup

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: { rules: [{ test: /\.scss$/i, use: ['style-loader', 'css-loader', 'sass-loader'], },], }
+}
