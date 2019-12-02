d28dde4623f55449e58a16162a5c9981b356a833
imhta <imhtapm@gmail.com>
2019-12-02 22:01:31 +0530

    :heavy_plus_sign:  add fav btn for add todo style and layout with some :up: responsive width for home wrapper

--- src/components/main-card.js
+++ src/components/main-card.js
@@ -3,8 +3,15 @@ import todoItem from './todo-item';
 const MainCard = (() => `
     <div id='main-card'>
         <ul id='todo-list'>
-            ${[...Array(11).keys()].map((i) => todoItem(i)).join('')}
+            ${[...Array(11).keys()].map(i => todoItem(i)).join('')}
         </ul>
+        <div class="btn-wrapper">
+            <button id='add-todo-btn'>
+                <i class='material-icons'>
+                    add
+                </i>
+            </button>
+        </div>
     </div>
 `)();
 
