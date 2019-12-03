e10f4e7ba24793d2ac3e560134c6f52e0c0d2773
imhta <imhtapm@gmail.com>
2019-12-03 15:29:51 +0530

    :art:  :zap:  improve animation triger in scss

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,11 @@
 const Navbar = (() => `
     <nav>
         <div class='nav-left'>
-            <i class='material-icons'>
-                menu
-            </i>
+            <a id='burger-btn'>
+                <i class='material-icons'>
+                    menu
+                </i>
+            </a>
             <h3 id='status'>today<h3>
         </div>
         <div class='nav-right'>
