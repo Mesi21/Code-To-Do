dfc59729beaa10c8a95124da73cc3b264d979169
Mesi <mse212002@gmail.com>
2019-12-03 18:39:10 +0200

    :construction: work in progress for internal functionalities for adding and deleting projects and todos

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import './style/style.scss';
 import UI from './controller/ui.controller';
+import Project from './model/project';
+import state from './state';
+import Todo from './model/todo';
 
 UI.init();
 
 const burgerBtn = document.getElementById('burger-btn');
 const menuWrapperElement = document.getElementById('menu-wrapper');
+const projectAddBtn = document.getElementById('project-add-btn');
+const todoAddBtn = document.getElementById('add-todo-btn');
 
 burgerBtn.addEventListener('click', event => {
   const isExpanded = 'is-menu-expanded';
@@ -14,5 +19,16 @@ burgerBtn.addEventListener('click', event => {
   } else {
     menuWrapperElement.className = 'is-menu-expanded';
   }
+  event.preventDefault();
+});
+
+projectAddBtn.addEventListener('click', event => {
+  UI.addProject(new Project('title'));
+  event.preventDefault();
+});
+
+todoAddBtn.addEventListener('click', event => {
+  state.current.projects[0].todos.push(new Todo('First Todo'));
+  state.set('projects', state.current.projects);
   event.preventDefault();
 });
--- src/model/todo.js
+++ src/model/todo.js
@@ -1,5 +1,5 @@
 export default class Todo {
-  constructor(title, isCompleted) {
+  constructor(title, isCompleted = false) {
     this.title = title;
     this.isCompleted = isCompleted;
   }
