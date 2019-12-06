d3a9387933a5cfaec8275b77cd59c8aae79d8740
imhta <imhtapm@gmail.com>
2019-12-06 23:43:51 +0530

    :up: add project using input
    Co-Authored-By: mesi21 <mesi21@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ const projectAddBtn = document.getElementById('project-add-btn');
 const todoAddBtn = document.getElementById('add-todo-btn');
 const todoList = document.getElementById('todo-list');
 const projectsList = document.getElementById('project-list');
+const projectInput = document.getElementById('project-name-input');
 
 burgerBtn.addEventListener('click', event => {
   const isExpanded = 'is-menu-expanded';
@@ -26,14 +27,18 @@ burgerBtn.addEventListener('click', event => {
 });
 
 projectAddBtn.addEventListener('click', event => {
-  Projects.addProject('title');
-  UI.refresh.projects();
-  event.preventDefault();
+  if (/^\S/.test(projectInput.value)) {
+    Projects.addProject(projectInput.value);
+    UI.refresh.projects();
+    projectInput.value = '';
+    event.preventDefault();
+  }
 });
 
 todoAddBtn.addEventListener('click', event => {
-  Projects.selectProject(state.get('selectedProject'))
-    .addTodo('want to buy milk');
+  Projects.selectProject(state.get('selectedProject')).addTodo(
+    'want to buy milk',
+  );
   UI.refresh.todos();
   event.preventDefault();
 });
@@ -46,7 +51,8 @@ projectsList.addEventListener('click', ({ target }) => {
 
 todoList.addEventListener('click', ({ target }) => {
   const todoIndex = Number(target.id.split('-')[1]);
-  Projects.selectProject(state.get('selectedProject'))
-    .toggleTodoCompleted(todoIndex);
+  Projects.selectProject(state.get('selectedProject')).toggleTodoCompleted(
+    todoIndex,
+  );
   UI.refresh.todos();
 });
